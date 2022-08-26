8f12d039f67d3ab13a4fdaa8ce0c2f513cbf29c2
Adrienne <100542795+ampando@users.noreply.github.com>
2022-08-26 10:29:17 -0700

    create addFlavor route and view in treats controller.

--- Treats/Controllers/TreatsController.cs
+++ Treats/Controllers/TreatsController.cs
@@ -82,6 +82,24 @@ namespace Treats.Controllers
       return RedirectToAction("Index");
     }
 
+    public ActionResult AddCategory(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+    public ActionResult AddCategory(Treat treat, int FlavorId)
+    {
+      if (FlavorId != 0)
+      {
+        _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Index");
+    }
+
     [Authorize]
     public ActionResult Delete(int id)
     {
