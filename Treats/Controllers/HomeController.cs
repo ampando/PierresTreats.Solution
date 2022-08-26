3d87582552f81c67dd143866a16a6d89e92cc615
Adrienne <100542795+ampando@users.noreply.github.com>
2022-08-26 11:04:47 -0700

    create details route and view for home controller.

--- Treats/Controllers/HomeController.cs
+++ Treats/Controllers/HomeController.cs
@@ -1,13 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Treats.Models;
+using System.Linq;
 
 namespace Treats.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly TreatsContext _db;
+    public HomeController( TreatsContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
+      return View();
+    }
+
+    [HttpGet("/Details")]
+    public ActionResult Details()
+    {
+      ViewBag.Flavors = _db.Flavors.ToList();
+      ViewBag.Treats = _db.Treats.ToList();
       return View();
     }
   }
